bd703a84917d7ce64da605a1588d386154901187
faslash <bicadete@hotmail.com>
2020-10-02 14:20:07 -0300

    Aula 45

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,15 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
 
-export default props => 
-    <button className="button">{props.label}</button>
+export default (props) => (
+  <button
+    onClick={e => props.click && props.click(props.label)}
+    className={`
+    button ${props.operation ? "operation" : ""}
+    button ${props.double ? "double" : ""}
+    button ${props.triple ? "triple" : ""}
+    `}
+  >
+    {props.label}
+  </button>
+);
